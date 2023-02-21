8782ba2dd6d6abf0497c7cbaa1cd0d0e7285c7aa
addpadding <tiktokaddison20@gmail.com>
2023-02-21 20:26:13 +0300

    lesson-31 = طريقة كتابة الجافاسكريبت

---
+++ js/app.js
@@ -0,0 +1,60 @@
+// document.write("Hello")
+
+// document.getElementById("result").innerHTML = "hello innerHTML";
+
+document.getElementById("btn").onclick = function () {
+    document.getElementById("result").innerHTML = "hello innerHTML";
+}
+
+console.log("ddddd");
+
+// alert("ssssss")
+
+console.log(1 + 2);
+console.log("1 + 2");
+
+
+
+// Variables
+var x = 5;
+
+var num = 6 + 15;
+var num2 = 6;
+
+console.log(num)
+console.log(num2)
+
+var num3 = 5;
+console.log(typeof num3)
+
+var num4 = true;
+console.log(typeof num4)
+
+var num5 = [1.2, 3];
+console.log(typeof num5)
+
+
+var num7 = ["hamza", "yasser", "ahmed"];
+console.log(num7[1])
+console.log(typeof num7)
+
+
+
+var person = {
+    name: "test",
+    age: "46"
+};
+
+console.log(person.name)
+console.log(typeof person)
+
+
+
+var x2;
+
+console.log(x2)
+console.log(typeof x2)
+
+
+person = null
+console.log(typeof person)
