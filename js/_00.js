5afc6dc95e2b120b11b93331acfdee5b817cf1ff
addpadding <tiktokaddison20@gmail.com>
2023-02-23 05:13:05 +0300

    lesson-36 = 100% = Ajax & Objects

--- js/_00.js
+++ js/_00.js
@@ -1,6 +1,15 @@
 // ========= (000) =========
 
-console.log(Math.aaa(00));
+// ffffff
+var car_000 = {
+
+    name: "BMW",
+    year: "2014",
+
+}
+
+console.log(car_000.name)
+console.log(car_000.year)
 
 
-console.log("000 = fffffffff");
+console.log("000 = fff");
--- js/app.js
+++ js/app.js
@@ -1,32 +1,90 @@
-
 // ========= (001) =========
 
-function lodad_Data_001() {
-    // =========
-    var http_001 = new XMLHttpRequest();
-    // =========
-    console.log(http_001)
-    // =========
-    http_001.onreadystatechange = function () {
-        // =========
-        // console.log(this.readyState)
-        console.log(this.status)
-        // =========
-        if (this.readyState == 4 && this.status == 200) {
-            // =========
-            // console.log(this.responseText);
-            // console.log(typeof this.responseText);
-            console.log(JSON.parse(this.responseText));
-            // =========
-        }
-        // =========
-    }
-    // =========
-    // Asy-nch-ron-ous لا استنى
-    // synchronous  استنى
-    // =========
-    http_001.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-    http_001.send()
-    // =========
+// literals
+var car_001 = {
+
+    name: "BMW",
+    year: "2014",
+
 }
-lodad_Data_001()
+console.log(car_001.name)
+console.log(car_001.year)
+
+console.log(car_001["name"])
+console.log(car_001["year"])
+
+
+
+console.log("002 = literals");
+
+// ========= (002) =========
+
+// use new
+var book_002 = new Object()
+
+book_002.name = "javascript"
+book_002.author = "hamza"
+
+console.log(book_002)
+console.log(book_002.name)
+console.log(book_002["name"])
+
+
+console.log("002 = new");
+
+// ========= (003) =========
+
+// object.create
+var car_003 = Object.create(car_001, {
+    code: {
+        value: "123456"
+    }
+})
+
+console.log(car_003)
+
+
+
+console.log("003 = create");
+
+// ========= (004) =========
+
+// object.create
+
+var test_004 = {}
+
+var car_004 = Object.create(test_004, {
+    code: {
+        value: "123456"
+    }
+})
+
+console.log(car_004)
+
+
+
+console.log("004 = create test");
+
+// ========= (005) =========
+
+var student_005 = {}
+
+var data_005 = Object.create(student_005, {
+    name: {
+        value: "test"
+    },
+    sss: {
+        value: "yes"
+    }
+})
+
+var data_0055 = {
+    name: "test"
+}
+
+console.log(data_005)
+console.log(data_0055)
+
+
+
+console.log("005 = create student");
