a577b267a99c716d3d83696e35819749a75e709b
addpadding <tiktokaddison20@gmail.com>
2023-02-24 22:07:01 +0300

    lesson-41 = 100% = ES6 & let & const & Hoisting & arrow

--- js/app.js
+++ js/app.js
@@ -1,51 +1,157 @@
-console.log(window.innerWidth);
-console.log(window.innerHeight);
+// var x = 1;
+
+// var test = 12
+
+// //scope
+// var x = 2
+
+// function test_code() {
+//     var test = 14 // local
+// }
 
 
-var x = 10;
-console.log(x)
+// var x = 2
+// console.log(window.x)
 
-console.log(window.x)
+// let x2 = 2
+// console.log(window.x2)
 
-window.confirm(" do wont delete this item")
+// var x = 2
+// x = 5
 
-var x = window.confirm(" do wont delete this item")
-console.log(x)
+// console.log(x)
 
-var x = window.prompt(" title ")
-console.log(x)
+// let x2 = 2
+// x2 = 5
+
+// console.log(x2)
+
+// const x3 = 2
+// x3 = 5
+
+// console.log(x3)
+
+// const x = "hamza"
+// x = "yasser"
+
+// console.log(x)
+
+// const x = {
+//     time: 5
+// }
+
+// x.time = 8;
+
+// console.log(x)
+
+// var x = 5
+
+// var x = 2
+
+// console.log(x)
+
+// let x = 5
+
+// let x = 2
+
+// console.log(x)
+
+// const x = 5
+
+// const x = 2
+
+// console.log(x)
+
+// console.log(x)
+// var x = 1
+// console.log(x)
+
+// console.log(x)
+// let x = 1
 
 
-window.open("", "", "width=900, height=500")
 
-function add_window() {
-    window.open("", "", "width=900, height=500")
+// test()
+// function test() {
+//     var x = 1;
+//     console.log(x);
+// }
+// test()
+
+
+
+// test()
+// var test = function () {
+//     var x = 1;
+//     console.log(x);
+// }
+// test()
+
+
+
+// var x = 1;
+// function host() {
+//     if (false) {
+//         var x = 2;
+//     }
+//     console.log(x);
+// }
+// host()
+
+
+// let x = 1;
+// function host() {
+//     if (false) {
+//         let x = 2;
+//     }
+//     console.log(x);
+// }
+// host()
+
+
+// var host = 1;
+// function host() {
+//     console.log("test");
+// }
+// console.log(typeof host)
+
+
+// var host;
+// function host() {
+//     console.log("test");
+// }
+// console.log(typeof host)
+
+
+// var host;
+// function host() {
+//     console.log("test");
+// }
+// console.log(typeof host)
+
+//------------------- 01
+function arrow_1(x) {
+    console.log(x)
 }
+arrow_1(1)
 
-
-let popup;
-
-function open_window() {
-    popup = window.open("https://www.google.com/", "", "width=900, height=500")
+//------------------- 02
+var arrow_2 = (x) => {
+    console.log(x);
 }
+arrow_2(2)
 
-function close_window() {
-    popup.close()
+//------------------- 03
+var arrow_3 = x => {
+    console.log(x);
 }
+arrow_3(3)
 
-document.querySelector(".open").addEventListener("click", open_window)
-document.querySelector(".close").addEventListener("click", close_window)
+//------------------- 04
+var arrow_4 = x => console.log(x);
+arrow_4(4)
 
-
-console.log(screen.width);
-console.log(screen.availWidth);
-
-console.log(screen.height);
-console.log(screen.availHeight);
-
-console.log(navigator.appName);
-console.log(navigator.appCodeName);
-console.log(navigator.platform);
-console.log(navigator.appVersion);
-console.log(navigator.language);
-console.log(navigator.onLine);
+//------------------- 05
+var arrow_5 = (x, y) => console.log(x, y);
+arrow_5(6, 10)
+//-------------------
