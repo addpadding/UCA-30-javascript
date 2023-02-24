03465c6ff9acf42bd34c824c00c3447f48b484fb
addpadding <tiktokaddison20@gmail.com>
2023-02-24 18:54:26 +0300

    lesson-40 = 100% = BOM

--- js/_00.js
+++ js/_00.js
@@ -1,23 +1,10 @@
 // ========= (000) =========
 
 
-function fun_000() {
-    // =========
-    console.log("result_000");
-    // =========
-}
-// =========
-fun_000
-
-var btn_000 = document.getElementById("btn_000")
-
-var demo_000 = document.getElementById("demo_000")
-
-demo_000.addEventListener("click", fun_000)
 
 
 
-console.log("---000---mouse-fff");
+console.log("---000---");
 // -----------------------------------------------
 
 
--- js/app.js
+++ js/app.js
@@ -1,128 +1,51 @@
-var product_btn = document.getElementById("product_btn")
-var product_container = document.getElementById("product_container")
+console.log(window.innerWidth);
+console.log(window.innerHeight);
 
-var data = [
 
-    {
-        id: 1,
-        name: "product 01",
-        desc: "pal pla pla"
-    },
+var x = 10;
+console.log(x)
 
-    {
-        id: 2,
-        name: "product 02",
-        desc: "pal pla pla"
-    },
+console.log(window.x)
 
-    {
-        id: 3,
-        name: "product 03",
-        desc: "pal pla pla"
-    },
+window.confirm(" do wont delete this item")
 
-    {
-        id: 4,
-        name: "product 04",
-        desc: "pal pla pla"
-    }
+var x = window.confirm(" do wont delete this item")
+console.log(x)
 
-]
+var x = window.prompt(" title ")
+console.log(x)
 
-// draw_Ui
-function draw_Ui(items) {
-    // =
-    product_container.innerHTML = "";
-    // =
-    items.forEach(function (ele) {
-        // =
-        // console.log(ele)
-        // product_container.innerHTML += "<div>" + ele.name + "</div>";
 
-        product_container.innerHTML += `
-        <div onclick="delete_item_fun(${ele.id})">
-        ${ele.name}
-        </div>
-        `;
-    })
-    // =
+window.open("", "", "width=900, height=500")
+
+function add_window() {
+    window.open("", "", "width=900, height=500")
 }
 
-// load data
-window.onload = function () {
-    // =
-    draw_Ui(data)
-    // =
+
+let popup;
+
+function open_window() {
+    popup = window.open("https://www.google.com/", "", "width=900, height=500")
 }
 
-// add item
-product_btn.addEventListener("click", add_item)
-
-function add_item() {
-    // =
-    var product_input = document.getElementById("product_input")
-
-    // =
-    if (product_input.value == "") {
-        // =
-        alert("you should enter product")
-        // =
-    }
-
-    // =
-    var last_id = data.length ? data[data.length - 1].id : 0;
-
-    console.log(++last_id)
-
-    // add to array
-    data.push(
-        {
-            id: ++last_id,
-            name: product_input.value,
-            desc: "pal pla pla"
-        }
-    )
-
-    // =
-    console.log(data);
-
-    // =
-    var add_item = data[data.length - 1]
-    console.log(add_item)
-
-    // =
-    // product_container.innerHTML += "<div>" + add_item.name + "</div>";
-    product_container.innerHTML += `
-        <div onclick="delete_item_fun(${add_item.id})">
-        ${add_item.name}
-        </div>
-        `;
-
-    // =
-    product_input.value = "";
-
+function close_window() {
+    popup.close()
 }
 
-function delete_item_fun(id_item) {
-    // console.log(id_item)
+document.querySelector(".open").addEventListener("click", open_window)
+document.querySelector(".close").addEventListener("click", close_window)
 
-    // data.splice(0, 1)
 
-    // =
-    var index = data.map(function (i_i) {
-        // =
-        return i_i.id
-    }).indexOf(id_item)
+console.log(screen.width);
+console.log(screen.availWidth);
 
-    // console.log(index)
+console.log(screen.height);
+console.log(screen.availHeight);
 
-    if (index !== 1) {
-        // -
-        data.splice(index, 1)
-        // -
-        draw_Ui(data)
-    }
-
-    console.log(data)
-
-}
+console.log(navigator.appName);
+console.log(navigator.appCodeName);
+console.log(navigator.platform);
+console.log(navigator.appVersion);
+console.log(navigator.language);
+console.log(navigator.onLine);
