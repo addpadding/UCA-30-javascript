5d45408694240637379d9f7a151f0fcb6e18c027
addpadding <tiktokaddison20@gmail.com>
2023-02-27 16:57:28 +0300

    lesson-48 = 100% = OOP شرح الـ part 5 last

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
+
+
+// =
+// =
+// =
+// =
+// =
+
 // ======== (01) ========
 
-class Student_01 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-}
+const carr_01 = new WeakMap();
 
-const obj_st_01 = new Student_01("hamza", 20)
-console.log(obj_st_01)
+const obj_01 = { a: "b" }
+
+carr_01.set(obj_01, "hamza")
+
+console.log(carr_01.get(obj_01))
+
 
 
 // ========================================
@@ -23,20 +30,23 @@ console.log("---01---");
 
 // ======== (02) ========
 
-class Student_02 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+const _model_02 = new WeakMap();
 
-        this.info = function () {
-            console.log(`Name: ${this.name} and age : ${this.age}`);
-        }
+
+class Car_02 {
+    constructor(model) {
+        _model_02.set(this, model)
+    }
+
+    data_02() {
+        // _model_02.get(this)
+        console.log(_model_02.get(this))
     }
 }
 
-const obj_st_02 = new Student_02("hamza", 20)
-console.log(obj_st_02)
-obj_st_02.info()
+
+const obj_car_02 = new Car_02("2015");
+
 
 
 // ========================================
@@ -51,355 +61,104 @@ console.log("---02---");
 
 // ======== (03) ========
 
-class Student_03 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+const _model_03 = new WeakMap();
+
+
+class Car_03 {
+    constructor(model) {
+        _model_03.set(this, model)
     }
 
-    info() {
-        console.log(`Name: ${this.name} and age : ${this.age}`);
+    data_03() {
+        console.log(_model_03.get(this))
     }
 }
 
-const obj_st_03 = new Student_03("hamza", 20)
-console.log(obj_st_03)
-obj_st_03.info()
 
-console.log(typeof Student_03)
+const obj_car_03 = new Car_03("2015");
+console.log(obj_car_03.data_03())
+
+
 
 // ========================================
 console.log("---03---");
 // ========================================
 
-// =
-// =
-// =
-// =
-// =
+/*
+=
+=
+=
+=
+=
+*/
 
 // ======== (04) ========
 
-class Student_04 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+const _model_04 = new WeakMap();
+const _run_04 = new WeakMap();
+
+
+class Car_04 {
+    constructor(model) {
+        _model_04.set(this, model)
+
+        _run_04.set(this, () => {
+            console.log(" run car");
+        })
     }
 
-    info() {
-        console.log(`Name: ${this.name} and age : ${this.age}`);
+    data_04() {
+        // console.log(_model_04.get(this))
+        _run_04.get(this)()
     }
 }
 
-const obj_st_04 = new Student_04("hamza", 20)
-console.log(obj_st_04)
-obj_st_04.info()
 
-console.log(typeof Student_04)
+const obj_car_04 = new Car_04("2015");
+console.log(obj_car_04.data_04())
 
 
-function hamada_1() { }
-
-const hamada_2 = function () { }
-
-class hamada_3 { }
-
-const hamada_4 = class { }
 
 // ========================================
 console.log("---04---");
 // ========================================
 
-// =
-// =
-// =
-// =
-// =
+/*
+=
+=
+=
+=
+=
+*/
 
 // ======== (05) ========
 
-class Math {
+const _model_05 = new WeakMap();
+const _run_05 = new WeakMap();
 
-    static abs() { }
 
-    abs() { }
-
-}
-
-Math.abs()
-
-class Student_05 {
-
-    test = 1;
-
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+class Car_05 {
+    constructor(model) {
+        _model_05.set(this, model)
     }
 
-    info() {
+    get model_get() {
+        console.log(_model_05.get(this))
+    }
+
+    set model_get(value) {
+        _model_05.set(this, value)
     }
 }
 
-// Student_05.
 
-// const st = new Student_01()
-// st.
+const obj_car_05 = new Car_05("2015");
+
+// obj_car_05.model_get()
 
 
 // ========================================
 console.log("---05---");
-// ========================================
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (06) ========
-
-
-class Student_06 {
-
-    static number;
-    // static get_number() { }
-
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    info() {
-    }
-}
-
-// Student_06.get_number()
-
-// const st_06 = new Student_06()
-// st_06.get_number()
-
-
-// ========================================
-console.log("---06---");
-// ========================================
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (07) ========
-
-// "use strict";
-
-const Student_07 = function () {
-    this.info = function () {
-        console.log(this)
-    }
-}
-
-const st_07 = new Student_07();
-st_07.info(); // method call
-
-const s_07 = st_07.info;
-s_07(); // function call
-
-// ========================================
-console.log("---07---");
-// ========================================
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (08) ========
-
-
-class Student_08 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    info() {
-        console.log(this)
-    }
-}
-
-const st_08 = new Student_08();
-st_08.info();
-
-const s_08 = st_08.info;
-s_07();
-
-
-// ========================================
-console.log("---08---");
-// ========================================
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (09) ========
-
-
-class Student_09 {
-    constructor(name, age) {
-        let _name = name
-        let _age = age
-
-
-        this.setName = function (name) {
-            _name = name
-        }
-
-        this.setAge = function (age) {
-            _age = age
-        }
-
-        this.get_name = function () {
-            return _name
-        }
-
-        this.get_age = function () {
-            return _age
-        }
-
-    }
-
-}
-
-const obj_09 = new Student_09();
-obj_09.setName("hamza")
-obj_09.setAge(20)
-
-
-const new_name_09 = obj_09.get_name()
-const new_age_09 = obj_09.get_age()
-
-console.log(new_name_09, new_age_09)
-
-
-// console.log(obj_09)
-
-// ========================================
-console.log("---09---");
-// ========================================
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (10) ========
-
-
-class Person_10 {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
-
-    data_10() {
-        // console.log(this.name, this.age);
-        console.log("test");
-    }
-
-}
-
-class Student_10 extends Person_10 { }
-
-const obj_10 = new Student_10();
-
-obj_10.data_10()
-
-// console.log(obj_10)
-
-
-// ========================================
-console.log("---10---");
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (11) ========
-
-
-class Person_11 {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
-
-    data_11() {
-        console.log(this.name, this.age);
-    }
-
-}
-
-class Student_11 extends Person_11 {
-    constructor(name, age) {
-        super(name, age)
-    }
-}
-
-const obj_11 = new Student_11("hamza", 20);
-
-
-// ========================================
-console.log("---11---");
-// ========================================
-
-// =
-// =
-// =
-// =
-// =
-
-// ======== (12) ========
-
-
-class Person_12 {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
-
-    data_12() {
-        console.log(this.name, this.age);
-    }
-
-}
-
-class Student_12 extends Person_12 {
-    constructor(name, age) {
-        super(name, age)
-    }
-
-    data_12() {
-        super.data_12()
-        console.log("test");
-    }
-}
-
-const obj_12 = new Student_12("hamza", 20);
-
-obj_12.data_12()
-
-
-// ========================================
-console.log("---12---");
 // ========================================
 
 
