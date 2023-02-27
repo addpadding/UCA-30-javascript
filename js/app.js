94429992d6ba391b232a7781f98bfb256c70f78d
addpadding <tiktokaddison20@gmail.com>
2023-02-27 04:58:25 +0300

    lesson-46 = 100% = OOP شرح الـ part 3

--- js/app.js
+++ js/app.js
@@ -2,56 +2,38 @@
 
 // ======== (01) ========
 
-function F_01(name_item) {
-    this.name = name_item
+function cat_01() { }
 
-    let B_01_2 = function () {
-        console.log("prise is 200");
-    }
-
-    this.F_01_2 = function () {
-        console.log(`person name is ${this.name}`);
-        B_01_2()
-    }
+cat_01.prototype.walk = function () {
+    console.log("cat walk")
 }
 
-
-let B_01 = new F_01("hamza")
-B_01.F_01_2()
-
+let obj_01 = new cat_01()
+console.log(obj_01)
 // ========================================
 console.log("---01---");
 // ========================================
 
-
 // ======== (02) ========
 
-function person_02(name_item) {
-    this.name = name_item
+function cat_02() { }
 
-    this.data_02 = function () {
-        console.log(`person name is ${this.name}`);
-    }
+cat_02.prototype.walk = function () {
+    console.log("cat walk")
 }
 
-// function person_022(name_item) {
-//     this.name = name_item
 
-//     this.data_02 = function () {
-//         console.log(`person name is ${this.name}`);
-//     }
 
-//     this.sayHello = function () { }
-// }
+function Rabbit_02() { }
 
-function stu_02(name, age) {
-    this.age = age
-    person_02.call(this, name)
+Rabbit_02.prototype.walk = function () {
+    console.log("Rabbit walk")
 }
 
-let s1_02 = new stu_02("ahmed", 20)
 
 
+let obj_02 = new cat_02()
+console.log(obj_02)
 
 // ========================================
 console.log("---02---");
@@ -59,12 +41,29 @@ console.log("---02---");
 
 // ======== (03) ========
 
-function Student_03(name) {
-    this.name = name;
+function Animal_03() { }
+
+Animal_03.prototype.walk = function () {
+    console.log("cat walk")
 }
 
-let s1_03 = new Student_03("ahmed", 20);
+function cat_03() {
+    // this.run_03 = function () {
+    //     console.log("run_03")
+    // }
+}
 
+// Object.create(Animal_03.prototype)
+
+cat_03.prototype = Object.create(Animal_03.prototype)
+
+// cat_03.prototype.walk = function () {
+//     console.log("cat walk")
+// }
+
+
+let obj_03 = new cat_03()
+console.log(obj_03)
 
 // ========================================
 console.log("---03---");
@@ -72,52 +71,50 @@ console.log("---03---");
 
 // ======== (04) ========
 
-let obj_04 = {
-    title: "JavaScript",
-    test: function () {
-        console.log("test code");
-    }
+function Animal_04() { }
+Animal_04.prototype.walk = function () {
+    console.log("cat walk")
 }
+
+function cat_04() { }
+
+cat_04.prototype = Object.create(Animal_04.prototype)
+cat_04.prototype.constructor = cat_04
+cat_04.prototype.run_04 = function () {
+    console.log("run_04")
+}
+
+
+
+let obj_04 = new cat_04()
+console.log(obj_04)
 
 // ========================================
 console.log("---04---");
 // ========================================
 
-// ======== (05) ========
-
-let a_05 = {
-    title: "java"
-}
-
-// Object.keys(a_05)
-console.log(Object.keys(a_05))
-
-let per_obj_05 = Object.getPrototypeOf(a_05)
-
-let desc_05 = Object.getOwnPropertyDescriptor(per_obj_05, "toString")
-console.log(desc_05)
-
-// ========================================
-console.log("---05---");
-// ========================================
-
 // ======== (06) ========
 
-let a_06 = {
-    title: "java"
-};
+function Animal_06(name) {
+    this.name = name
+}
 
-Object.defineProperty(a_06, "title", {
-    configurable: false,
-    writable: true,
-    enumerable: true,
-})
+Animal_06.prototype.walk = function () {
+    console.log("cat walk")
+}
 
-// delete a_06.title
-a_06.title = "css"
+function cat_06(name) {
+    Animal_06.call(this, name)
+}
 
-// console.log(a_06)
-console.log(Object.keys(a_06))
+cat_06.prototype = Object.create(Animal_06.prototype)
+cat_06.prototype.constructor = cat_06
+
+
+
+
+let obj_06 = new cat_06("cat")
+console.log(obj_06)
 
 // ========================================
 console.log("---06---");
@@ -125,32 +122,281 @@ console.log("---06---");
 
 // ======== (07) ========
 
-function person_07(name) {
+function Animal_07(name) {
     this.name = name
 }
 
-let p_07 = new person_07("ahmed")
+Animal_07.prototype.walk = function () {
+    console.log("cat walk")
+}
 
-person_07.prototype = p_07.__proto__
+function cat_07(name) {
+    Animal_07.call(this, name)
+}
+
+cat_07.prototype = Object.create(Animal_07.prototype)
+cat_07.prototype.constructor = cat_07
+// ==============
+
+function Rabbit_07(name) {
+    Animal_07.call(this, name)
+}
+
+Rabbit_07.prototype = Object.create(Animal_07.prototype)
+Rabbit_07.prototype.constructor = Rabbit_07
+// ==============
+
+
+
+
+let obj_07 = new cat_07("cat")
+let obj_07_2 = new Rabbit_07("rabbit")
+console.log(obj_07)
+console.log(obj_07_2)
 
 // ========================================
-
 console.log("---07---");
 // ========================================
 
 // ======== (08) ========
 
-function person_08(name) {
+function extend_08(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype)
+    Child.prototype.constructor = Child
+}
+
+function Animal_08(name) {
     this.name = name
 }
 
-let p_08 = new person_08("ahmed")
+Animal_08.prototype.walk = function () {
+    console.log("cat walk")
+}
 
-person_08.prototype = p_08.__proto__
+function cat_08(name) {
+    Animal_08.call(this, name)
+}
+
+extend_08(cat_08, Animal_08)
+// ==============
+
+function Rabbit_08(name) {
+    Animal_08.call(this, name)
+}
+
+extend_08(Rabbit_08, Animal_08)
+// ==============
+
+
+
+
+let obj_08 = new cat_08("cat")
+let obj_08_2 = new Rabbit_08("rabbit")
+console.log(obj_08)
+console.log(obj_08_2)
 
 // ========================================
-
 console.log("---08---");
+// ========================================
+
+// ======== (09) ========
+
+function extend_09(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype)
+    Child.prototype.constructor = Child
+}
+
+function Animal_09(name) {
+    this.name = name
+}
+
+Animal_09.prototype.walk = function () {
+    console.log("walk")
+}
+
+function cat_09(name) {
+    Animal_09.call(this, name)
+}
+
+extend_09(cat_09, Animal_09)
+
+cat_09.prototype.walk = function () {
+    console.log("cat walk")
+}
+// ==============
+
+function Rabbit_09(name) {
+    Animal_09.call(this, name)
+}
+
+extend_09(Rabbit_09, Animal_09)
+
+Rabbit_09.prototype.walk = function () {
+    console.log("Rabbit walk")
+}
+// ==============
+
+
+
+
+let obj_cat_09 = new cat_09("cat")
+let obj_rabbit_09 = new Rabbit_09("rabbit")
+console.log(obj_cat_09)
+console.log(obj_rabbit_09)
+
+// ========================================
+console.log("---09--- overriding");
+// ========================================
+
+// ======== (10) ========
+
+let can_walk_10 = {
+    walk: function () {
+        console.log("walk")
+    }
+}
+
+let can_swim_10 = {
+    swim: function () {
+        console.log("swim")
+    }
+}
+
+// ====(cat)====
+function Cat_10(name) {
+    this.name = name
+}
+
+
+Object.assign(Cat_10.prototype, can_walk_10)
+// ===
+
+// ====(fish)====
+function fish_10(name) {
+    this.name = name
+}
+
+
+Object.assign(fish_10.prototype, can_swim_10)
+// ===
+
+const obj_cat_10 = new Cat_10("cat")
+const obj_fish_10 = new fish_10("fish")
+console.log(obj_cat_10)
+console.log(obj_fish_10)
+
+
+
+// ========================================
+console.log("---10---");
+// ========================================
+
+// ======== (11) ========
+
+// walk
+let can_walk_11 = {
+    walk: function () {
+        console.log("walk")
+    }
+}
+
+// run
+let can_run_11 = {
+    run: function () {
+        console.log("run")
+    }
+}
+
+// swim
+let can_swim_11 = {
+    swim: function () {
+        console.log("swim")
+    }
+}
+
+// ====(cat)====
+function Cat_11(name) {
+    this.name = name
+}
+
+
+Object.assign(Cat_11.prototype, can_walk_11, can_run_11)
+// ===
+
+// ====(fish)====
+function fish_11(name) {
+    this.name = name
+}
+
+
+Object.assign(fish_11.prototype, can_swim_11, can_run_11)
+// ===
+
+const obj_cat_11 = new Cat_11("cat")
+const obj_fish_11 = new fish_11("fish")
+console.log(obj_cat_11)
+console.log(obj_fish_11)
+
+
+
+// ========================================
+console.log("---11---");
+// ========================================
+
+// ======== (12) ========
+
+function mixin_12(obj, ...params) {
+    Object.assign(obj, ...params)
+}
+
+// walk
+let can_walk_12 = {
+    walk: function () {
+        console.log("walk")
+    }
+}
+
+// run
+let can_run_12 = {
+    run: function () {
+        console.log("run")
+    }
+}
+
+// swim
+let can_swim_12 = {
+    swim: function () {
+        console.log("swim")
+    }
+}
+
+// ====(cat)====
+function Cat_12(name) {
+    this.name = name
+}
+
+
+mixin_12(Cat_12.prototype, can_walk_12, can_run_12)
+// ===
+
+// ====(fish)====
+function fish_12(name) {
+    this.name = name
+}
+
+
+mixin_12(fish_12.prototype, can_swim_12, can_run_12)
+// ===
+
+const obj_cat_12 = new Cat_12("cat")
+const obj_fish_12 = new fish_12("fish")
+console.log(obj_cat_12)
+console.log(obj_fish_12)
+
+
+
+// ========================================
+console.log("---12---");
 // ========================================
 
 
